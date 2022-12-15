613758aefe630669b69007b121a805101ec8f8a2
NOURA_ALGAHTANI <Nourahaniq@gmail.com>
2022-12-15 19:50:20 +0300

    just  js file

---
+++ Dashoard/Js/class.js
@@ -0,0 +1,37 @@
+const COURSE_DATA = [
+    {
+        Class: "Calculus",
+        instructer: "jacob smith",
+        classnumber: "SE392",
+        status: "pending",
+        statuscolor: "warning",
+    },
+    {
+        Class: "History",
+        instructer: "laurie wil",
+        classnumber: "SE762",
+        status: "pending",
+        statuscolor: "warning",
+    },
+    {
+        Class: "History",
+        instructer: "laurie wil",
+        classnumber: "SE762",
+        status: "pending",
+        statuscolor: "warning",
+    },
+    {
+        Class: "History",
+        instructer: "laurie wil",
+        classnumber: "SE762",
+        status: "pending",
+        statuscolor: "warning",
+    },
+    {
+        Class: "History",
+        instructer: "laurie wil",
+        classnumber: "SE762",
+        status: "pending",
+        statuscolor: "warning",
+    },
+];
